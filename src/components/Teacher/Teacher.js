357af2f517f9cea377e9d42cd750d76967579cd2
Shipu8644 <shipu.monjurur@gmail.com>
2021-10-04 15:35:01 +0600

    teacher style done

--- src/components/Teacher/Teacher.js
+++ src/components/Teacher/Teacher.js
@@ -4,16 +4,29 @@ const Teacher = (props) => {
     const { name, age, gender, followers, img } = props.teacher;
     return (
         <div>
-            <div className='grid grid-cols-6 gap-5 mb-4 bg-indigo-100 align-middle border-blue-900 border-2 rounded-lg'>
+            {/* <div className='grid grid-cols-6 gap-5 mb-4 bg-indigo-100 align-middle border-blue-900 border-2 rounded-lg'>
                 <div >
                     <img className='w-50 h-20 rounded ' src={img} alt="" />
                 </div>
 
-                <h1 className='pt-5'>Name: {name}</h1>
-                <h1 className='pt-5'>Age: {age}</h1>
-                <h1 className='pt-5'>Gender: {gender}</h1>
-                <h1 className='pt-5'>Popularity: {followers}</h1>
+                <h1 >Name: {name}</h1>
+                <h1 >Age: {age}</h1>
+                <h1 >Gender: {gender}</h1>
+                <h1 >Popularity: {followers}</h1>
 
+            </div> */}
+
+            <div className="flex items-center p-4 bg-white border-2 
+            border-gray-200 rounded-lg  dark:bg-gray-800 w-2/3 ml-48 mt-4 mb-4 shadow-xl">
+                <img alt="food" className="w-1/3  rounded-md border-2 border-gray-300 " src={img} />
+                <div id="body" className="flex flex-col ml-5">
+                    <h4 id="name" className="text-xl font-semibold mb-2">{name}</h4>
+                    <h4 className=' font-serif'>Gender: {gender}</h4>
+                    <p className="text-gray-800 mt-2">
+                        Age: {age}</p>
+                    <p>Follower: {followers}</p>
+
+                </div>
             </div>
         </div>
 
