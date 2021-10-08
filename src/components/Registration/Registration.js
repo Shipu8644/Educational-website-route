14ec72777ed4190c0d65c96c7b2a087eb8f421c9
Shipu8644 <shipu.monjurur@gmail.com>
2021-10-08 19:27:15 +0600

    pass firebase done

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, createUserWithEmailAndPassword } from "firebase/auth";
 
 import intializeAuthentication from '../../Firebase/firebase.initialize';
@@ -8,8 +8,10 @@ const googleProvider = new GoogleAuthProvider();
 const githubProvider = new GithubAuthProvider();
 
 const Registration = () => {
-
     const auth = getAuth();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPass] = useState('');
 
     //  Google Sign In
     const handleGoogleSignIn = () => {
@@ -38,10 +40,35 @@ const Registration = () => {
     }
 
     //    passWord Sign up and In
+    const namehandle = (e) => {
+        // console.log(e.target.value);
+        setName(e.target.value);
+    }
+
+    const emailHandle = (e) => {
+
+        setEmail(e.target.value);
+    }
+
+    const passHandle = (e) => {
+
+        setPass(e.target.value);
+    }
+
+
     const handleSignUp = (e) => {
         e.preventDefault();
-        console.log(7777);
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => {
+                console.log(error.message);
+            })
     }
+
+
 
 
     return (
@@ -50,6 +77,7 @@ const Registration = () => {
 
             <form onSubmit={handleSignUp} className="flex items-center justify-center">
                 <div>
+                    {/* Name section */}
                     <div className="md:flex md:items-center mb-6">
                         <div className="md:w-1/3">
                             <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name">
@@ -57,9 +85,11 @@ const Registration = () => {
                             </label>
                         </div>
                         <div className="md:w-2/3">
-                            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="text" placeholder="Your name" />
+                            <input onBlur={namehandle} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="text" placeholder="Your name" />
                         </div>
                     </div>
+
+                    {/* Email Section */}
                     <div className="md:flex md:items-center mb-6">
                         <div className="md:w-1/3">
                             <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name">
@@ -67,9 +97,11 @@ const Registration = () => {
                             </label>
                         </div>
                         <div className="md:w-2/3">
-                            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="text" placeholder="Your email" />
+                            <input onBlur={emailHandle} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="text" placeholder="Your email" />
                         </div>
                     </div>
+
+                    {/* Password Section */}
                     <div className="md:flex md:items-center mb-6">
                         <div className="md:w-1/3">
                             <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-password">
@@ -77,7 +109,7 @@ const Registration = () => {
                             </label>
                         </div>
                         <div className="md:w-2/3">
-                            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="******************" />
+                            <input onBlur={passHandle} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="******************" />
                         </div>
                     </div>
                     <div className="md:flex md:items-center mb-6">
